97012e1e514c058badbb83688d210bb781ade12f
vipinnishad20 <vipinnishad.er@gmail.com>
2023-10-05 21:02:36 +0530

    commited

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const express = require('express');
+const server = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+const { createProduct } = require('./controller/Product');
+const productsRouter = require('./routes/Products');
+const categoriesRouter = require('./routes/Categories');
+const brandsRouter = require('./routes/Brands');
+const usersRouter = require('./routes/Users');
+const authRouter = require('./routes/Auth');
+const cartRouter = require('./routes/Cart');
+const ordersRouter = require('./routes/Order');
+
+
+//middlewares
+
+server.use(cors({
+    exposedHeaders:['X-Total-Count']
+}))
+server.use(express.json()); // to parse req.body
+server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
+server.use('/products', productsRouter.router);
+server.use('/categories', categoriesRouter.router)
+server.use('/brands', brandsRouter.router)
+server.use('/users', usersRouter.router)
+server.use('/auth', authRouter.router)
+server.use('/cart', cartRouter.router)
+server.use('/orders', ordersRouter.router)
+
+main().catch(err=> console.log(err));
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('database connected')
+}
+
+server.get('/',(req, res)=>{
+    res.json({status:'success'})
+})
+
+
+
+server.listen(process.env.PORT, ()=>{
+    console.log('server started')
+})
